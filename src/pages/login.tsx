673d50f2dd4475495c1b1897a84339f6f8c48379
resultfulness <alicja.rrosiak@gmail.com>
2026-01-21 09:04:35 +0100

    add login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,9 @@
+import "./login.css";
 import LoginForm from "../components/organisms/login-form";
 
 export default function Login() {
     return (
-        <div>
+        <div className="login-page">
             <LoginForm />
         </div>
     );
