27a50b3c1cc506c1be98ba731b5d6f774b50ddf0
resultfulness <alicja.rrosiak@gmail.com>
2026-01-21 12:42:44 +0100

    add light theme icons

--- src/components/atoms/icon.tsx
+++ src/components/atoms/icon.tsx
@@ -9,27 +9,49 @@ interface IconProps {
         | "sun"
         | "moon";
     size?: number;
+    theme?: "light" | "dark";
 }
 
-export default function Icon({ icon, size = 24 }: IconProps) {
+export default function Icon({ icon, size = 24, theme = "dark" }: IconProps) {
     function iconFile() {
-        switch (icon) {
-            case "arrow-up":
-                return "/arrow-up.svg";
-            case "arrow-down":
-                return "/arrow-down.svg";
-            case "arrow-left":
-                return "/arrow-left.svg";
-            case "arrow-right":
-                return "/arrow-right.svg";
-            case "sort":
-                return "/sort.svg";
-            case "sort-reverse":
-                return "/sort-reverse.svg";
-            case "sun":
-                return "/sun.svg";
-            case "moon":
-                return "/moon.svg";
+        if (theme === "dark") {
+            switch (icon) {
+                case "arrow-up":
+                    return "/arrow-up.svg";
+                case "arrow-down":
+                    return "/arrow-down.svg";
+                case "arrow-left":
+                    return "/arrow-left.svg";
+                case "arrow-right":
+                    return "/arrow-right.svg";
+                case "sort":
+                    return "/sort.svg";
+                case "sort-reverse":
+                    return "/sort-reverse.svg";
+                case "sun":
+                    return "/sun.svg";
+                case "moon":
+                    return "/moon.svg";
+            }
+        } else {
+            switch (icon) {
+                case "arrow-up":
+                    return "/light--arrow-up.svg";
+                case "arrow-down":
+                    return "/light--arrow-down.svg";
+                case "arrow-left":
+                    return "/light--arrow-left.svg";
+                case "arrow-right":
+                    return "/light--arrow-right.svg";
+                case "sort":
+                    return "/light--sort.svg";
+                case "sort-reverse":
+                    return "/light--sort-reverse.svg";
+                case "sun":
+                    return "/light--sun.svg";
+                case "moon":
+                    return "/light--moon.svg";
+            }
         }
     }
     return <img src={iconFile()} alt={icon} width={size} height={size} />;
