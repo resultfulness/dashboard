a865b90aac7585b3621e26d3184550e1a26c8502
resultfulness <alicja.rrosiak@gmail.com>
2026-01-21 12:34:47 +0100

    add icons

--- src/components/atoms/icon.tsx
+++ src/components/atoms/icon.tsx
@@ -1,3 +1,36 @@
-export default function Icon() {
-    return <div>Icon</div>;
+interface IconProps {
+    icon:
+        | "arrow-up"
+        | "arrow-down"
+        | "arrow-left"
+        | "arrow-right"
+        | "sort"
+        | "sort-reverse"
+        | "sun"
+        | "moon";
+    size?: number;
+}
+
+export default function Icon({ icon, size = 24 }: IconProps) {
+    function iconFile() {
+        switch (icon) {
+            case "arrow-up":
+                return "/arrow-up.svg";
+            case "arrow-down":
+                return "/arrow-down.svg";
+            case "arrow-left":
+                return "/arrow-left.svg";
+            case "arrow-right":
+                return "/arrow-right.svg";
+            case "sort":
+                return "/sort.svg";
+            case "sort-reverse":
+                return "/sort-reverse.svg";
+            case "sun":
+                return "/sun.svg";
+            case "moon":
+                return "/moon.svg";
+        }
+    }
+    return <img src={iconFile()} alt={icon} width={size} height={size} />;
 }
--- src/components/atoms/text-page-title.tsx
+++ src/components/atoms/text-page-title.tsx
@@ -1,3 +1,9 @@
-export default function TextPageTitle() {
-    return <div>TextPageTitle</div>;
+import "text-page-title.css";
+
+interface TextPageTitleProps {
+    children: React.ReactNode;
+}
+
+export default function TextPageTitle({ children }: TextPageTitleProps) {
+    return <h1 className="text-page-title">{children}</h1>;
 }
