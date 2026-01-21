9e0b927aea8d9dee132a5691220e85986b251a2f
resultfulness <alicja.rrosiak@gmail.com>
2026-01-21 09:12:02 +0100

    login page translations

--- src/translations/en.ts
+++ src/translations/en.ts
@@ -1,5 +1,9 @@
 const translation = {
     "reviews": "Reviews",
+    "login to the dashboard": "Dashboard login",
+    "username": "Username",
+    "password": "Password",
+    "log in": "Log in",
 } as const;
 
 export default { translation };
--- src/translations/pl.ts
+++ src/translations/pl.ts
@@ -1,5 +1,9 @@
 const translation = {
     "reviews": "Opinie",
+    "login to the dashboard": "Zaloguj się do panelu",
+    "username": "Nazwa użytkownika",
+    "password": "Hasło",
+    "log in": "Zaloguj",
 } as const;
 
 export default { translation };
