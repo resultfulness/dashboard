3a6d61397ed59f8596e03636b056b4bc64347a7a
resultfulness <alicja.rrosiak@gmail.com>
2026-01-21 08:49:44 +0100

    add heading component

--- src/components/atoms/text-heading.tsx
+++ src/components/atoms/text-heading.tsx
@@ -1,3 +1,27 @@
-export default function TextHeading() {
-    return <div>TextHeading</div>;
+interface TextHeadingProps {
+    children: React.ReactNode;
+    level: 1 | 2 | 3 | 4 | 5 | 6;
+}
+
+export default function TextHeading({ children, level }: TextHeadingProps) {
+    return (
+        <>
+            {(() => {
+                switch (level) {
+                    case 1:
+                        return <h1 className="text-heading">{children}</h1>;
+                    case 2:
+                        return <h2 className="text-heading">{children}</h2>;
+                    case 3:
+                        return <h3 className="text-heading">{children}</h3>;
+                    case 4:
+                        return <h4 className="text-heading">{children}</h4>;
+                    case 5:
+                        return <h5 className="text-heading">{children}</h5>;
+                    case 6:
+                        return <h6 className="text-heading">{children}</h6>;
+                }
+            })()}
+        </>
+    );
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,9 +1,13 @@
 import Button from "../components/atoms/button";
 import Input from "../components/atoms/input";
+import TextHeading from "../components/atoms/text-heading";
 
 export default function Login() {
-    return <div>
-        <Input />
-        <Button>Button</Button>
-    </div>;
+    return (
+        <div>
+            <TextHeading level={2}>Heading</TextHeading>
+            <Input />
+            <Button>Button</Button>
+        </div>
+    );
 }
