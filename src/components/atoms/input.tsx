89f17945d1cda1470808130d2b417495036da2d2
resultfulness <alicja.rrosiak@gmail.com>
2026-01-21 08:13:18 +0100

    add input component

--- src/components/atoms/input.tsx
+++ src/components/atoms/input.tsx
@@ -1,3 +1,27 @@
-export default function Input() {
-    return <div>Input</div>;
+import type { ChangeEvent } from "react";
+import "./input.css";
+
+interface InputProps {
+    value?: string;
+    name?: string;
+    placeholder?: string;
+    onchange?: () => void;
+}
+
+export default function Input({
+    value,
+    name,
+    placeholder,
+    onchange,
+}: InputProps) {
+    return (
+        <input
+            className="input"
+            type="text"
+            value={value}
+            name={name}
+            placeholder={placeholder}
+            onChange={onchange}
+        />
+    );
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,7 +1,9 @@
 import Button from "../components/atoms/button";
+import Input from "../components/atoms/input";
 
 export default function Login() {
     return <div>
+        <Input />
         <Button>Button</Button>
     </div>;
 }
